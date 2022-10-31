799a91f0bec8753e6e584569b36d57393e5bc4cb
Anastasyia <moya_pochta_n.k@mail.ru>
2022-10-31 22:24:37 +0700

    Task 02

---
+++ Task_02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Поиск максимального
+int[] array = {7, 10, 21, 6, 1, 11};
+int max = int.MaxValue;
+max = array[0];
+for(int i = 0; i < array.Length; i++)
+{
+    if (max < array[i])
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine(max);
