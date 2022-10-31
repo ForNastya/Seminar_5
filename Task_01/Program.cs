379207a51b358306fe77710806dadb74b20a3ca4
Anastasyia <moya_pochta_n.k@mail.ru>
2022-10-31 22:24:12 +0700

    Task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Поиск минимума
+int[] array = {7, 10, 21, 6, 1, 11};
+int min = int.MinValue;
+min = array[0];
+for(int i = 0; i < array.Length; i++)
+{
+    if (min > array[i])
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine(min);
