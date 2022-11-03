ef3450e863aadcdb42e473b5bf4b56271b2b0abe
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-03 22:20:07 +0700

    Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Подсчёт количества вхождений элемента в массив
+int ElementArray(int[] array, int num = 7)
+{
+    int n = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i]==num) n++;
+    return n;
+}
