b8c9596e1007466ef908f42ea29177f5600b423e
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-03 22:20:29 +0700

    Task 11

---
+++ Task_11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Подсчёт количества чётных элементов в массив
+int EvenArray(int[] array)
+{
+    int n = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] % 2 == 0) n++;
+    return n;
+}
