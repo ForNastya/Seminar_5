ae8caea2ba9415e13516f1b26833b3721c783fae
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-04 00:39:52 +0700

    Task 18

---
+++ Task_18/Program.cs
@@ -0,0 +1,19 @@
+﻿// Функцию, которая проверяет является ли заданное число n полиндромом
+bool Palindrom(int N)
+{
+    string str = N.ToString();
+    char [] ar = str.ToCharArray();
+    int j = 0;
+    int a = ar.Length/2;
+    
+for(int i=0; i<a; i++)
+{
+    int n = ar[i];
+    int m =ar[ar.Length-1-i];
+	if (m==n)j++;
+}
+if (j==a)
+return true;
+else
+return false;
+}
