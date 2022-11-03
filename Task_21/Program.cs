b3179a54cae53c4fe337c392eadd8c1e0706e8f4
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-04 00:40:46 +0700

    Task 21

---
+++ Task_21/Program.cs
@@ -0,0 +1,8 @@
+﻿// Функцию, определяющую является ли число чётным, то есть возвращающую true, если число чётное, иначе - false
+bool ChetNum(int N)
+{
+    if(N % 2 == 0)
+    return true;
+    else
+    return false;
+}
