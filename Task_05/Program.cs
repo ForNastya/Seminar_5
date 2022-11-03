483ff518572e2f153aa86f045a2ef8770f45a0ca
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-03 22:18:29 +0700

    Task 05

---
+++ Task_05/Program.cs
@@ -0,0 +1,8 @@
+﻿// Поиск индекса заданного элемента в массиве, если такого элемента в массиве нет то возвращать -1
+int IndexArray(int[] array, int num = 7)
+{
+    for(int i = 0; i < array.Length; i++)
+        if(array[i]==num)
+        return i;
+    return -1;
+}
