fe658190801e3dcc166dd0f515b7cf57f34de5b9
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-04 00:39:10 +0700

    Task 16

---
+++ Task_16/Program.cs
@@ -0,0 +1,11 @@
+﻿// Функция, которая вычисляет факториал числа n
+double Factorial(int N)
+{
+    double Fact = 1; 
+    if(N==0 || N==1)
+    return 1;
+    else
+    for(int i = 1; i <= N; i++) 
+    Fact = Fact * i;
+return Fact;
+}
