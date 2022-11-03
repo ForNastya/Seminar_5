28f291bca0277f5b94e811ad934c469463bac5a7
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-03 22:21:46 +0700

    Task 14

---
+++ Task_14/Program.cs
@@ -0,0 +1,37 @@
+﻿// Проверка является ли массив отсортированным по возрастанию. Если массив отсортирован, то возвращать true, иначе - false.
+int[] array = {11, 5, 9, 1, 33};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    int n = 0;
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int  minPosition = i;
+        
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) n = n + 1;
+        }
+    }
+if(n == 0)
+{
+    Console.Write(true);
+}
+else
+{
+    Console.Write(false);
+}
+}
+PrintArray(array);
+SelectionSort(array);
