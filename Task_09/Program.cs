8564de9b1c82d9f66f9d62149c6809a4ab774107
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-03 22:19:49 +0700

    Task 09

---
+++ Task_09/Program.cs
@@ -0,0 +1,8 @@
+﻿// Подсчёт количества отрицательных элементов массива
+int NegativeArray(int[] array)
+{
+    int n = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] < 0) n++;
+    return n;
+}
