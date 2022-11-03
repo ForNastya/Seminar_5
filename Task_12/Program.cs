6c3659473e03c2588c2aae286f016824762ad233
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-03 22:21:03 +0700

    Task 12

---
+++ Task_12/Program.cs
@@ -0,0 +1,8 @@
+﻿// Подсчёт количества положительных элементов в массиве
+int PositiveArray(int[] array)
+{
+    int n = 0;
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] >= 0) n++;
+    return n;
+}
