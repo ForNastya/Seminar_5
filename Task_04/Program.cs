e863335b2bcf3591c8d080f739b5b4bdb1f0b0ce
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-03 22:17:59 +0700

    Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Поиск произведения элементов массива
+int MultArray(int[] array)
+{
+    int mult = 1;
+    for(int i = 0; i < array.Length; i++)
+    {
+        mult = mult * array[i];
+    }
+    return mult;
+}
