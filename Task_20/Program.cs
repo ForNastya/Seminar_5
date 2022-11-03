6fc0ada3ad15906bc1f27985f4f31b2c0b1bef32
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-04 00:40:27 +0700

    Task 20

---
+++ Task_20/Program.cs
@@ -0,0 +1,14 @@
+﻿// Функцию, определяющую является ли число простым, то есть возвращающую true, если число простое, иначе - false
+bool ProstNum(int N)
+{
+    int m = 0;
+    for(int i = 2; i < 10; i++)
+        if( N != i)
+        {
+            if(N % i == 0) m++;
+        }
+    if(m == 0)
+    return true;
+    else
+    return false;
+}
