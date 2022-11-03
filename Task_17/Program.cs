c48490c96c04fc6744709740e25cd0b0f9a60361
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-04 00:39:30 +0700

    Task 17

---
+++ Task_17/Program.cs
@@ -0,0 +1,12 @@
+﻿// Функцию, которая вычисляет сумму цифр произвольного целого числа n
+double SumNum(int N)
+{
+    int sum = 0;
+   while(N > 0)
+   {
+    int tail = N % 10;
+    N = N / 10;
+    sum = sum + tail;
+   }
+return sum;
+}
