925eaac27e9e6a2f4804d52d9d13aa5054a6a717
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-03 22:18:52 +0700

    Task 06

---
+++ Task_06/Program.cs
@@ -0,0 +1,8 @@
+﻿// Проверка наличия элемента в массиве. Возвращает true, если элемент в массиве есть, false – нет.
+bool PresenceArray(int[] array, int num = 7)
+{
+    for(int i = 0; i < array.Length; i++)
+        if(array[i]==num)
+        return true;
+    return false;
+}
