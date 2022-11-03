30c26b6f9c44b814713707f009e318344793ee33
Anastasyia <moya_pochta_n.k@mail.ru>
2022-11-03 22:19:13 +0700

    Task 07

---
+++ Task_07/Program.cs
@@ -0,0 +1,19 @@
+﻿// Печать массива на экран
+void FillArray(int[] array, int min, int max)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(min, max);
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+}
+
+Console.WriteLine("Введите размер массива ");
+
+int size = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int [size];
+FillArray(array, 1, 33);
+PrintArray(array);
